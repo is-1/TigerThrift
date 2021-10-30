609bcc61759d3836a25d3ac1520e2b824907f1f8
Katie Chou <katielchou@gmail.com>
2021-10-30 14:55:57 -0400

    requirements

--- create.py
+++ create.py
@@ -27,43 +27,103 @@ def main():
             with closing(connection.cursor()) as cursor:
 
                 #------------------------------------------------------- create items table
+                cursor.execute("DROP TABLE IF EXISTS items")
+
                 f = '%Y-%m-%d %H:%M:%S'
                 now = datetime.utcnow()
                 testDate = now.strftime(f)
-                cursor.execute("DROP TABLE IF EXISTS items")
                 cursor.execute('CREATE TABLE items ("itemid" int NOT NULL, type text, subtype text, size ' + 
-                'varchar(10), gender varchar(10), price money, condition text, brand text, "desc" text,' + 
+                'varchar(10), gender varchar(10), price money, color text, condition text, brand text, "desc" text,' + 
                 'posted timestamp, photolink text, status int)')
                 # INSERT TEST DATA!!!
                 stmt_str = ("INSERT INTO items "
                      + '(itemid, type, subtype, size, gender, price, condition, brand, "desc", posted, photolink, status) '
-                     + "VALUES (0, 't-shirt', 'crop', 'XS', 'F', 10.50, 'new',  'topshop', 'never worn', %s, null, 0)")
+                     + "VALUES (0, 't-shirt', 'crop', 'XS', 'F', 10.50, 'like new',  'topshop', 'never worn', %s, null, 0)")
                 cursor.execute(stmt_str, [testDate])
-                #     + "'The Practice of Programming',500)")
-                # cursor.execute("INSERT INTO books "
-                #     + "(isbn, title, quantity) "
-                #     + "VALUES ('234',"
-                #     + "'The C Programming Language',800)")
-                # cursor.execute("INSERT INTO books "
-                #     + "(isbn, title, quantity) "
-                #     + "VALUES ('345',"
-                #     + "'Algorithms in C',650)")
 
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+
+                stmt_str = ('INSERT INTO items '
+                + '(itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status) ' +
+                "VALUES (1, 'bottom', 'skirt', 'M', 'F', 10, 'white', 'brand new', 'Urban Outfitters', 'white pleated skirt', %s, 'https://www.google.com/', 1)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' + 
+                "VALUES (2, 'top', 'shirt', 'M', 'F', 10, 'white', 'gently used', 'Zara', 'white satin blouse', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' + 
+                "VALUES (3, 'top', 'blouse', 'M', 'F', 10, 'white', 'very used', 'Uniqlo', 'white turtleneck', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status) ' + 
+                "VALUES (4, 'top', 'tank', 'S', 'F', 8, 'white', 'gently used', 'Aritzia', 'white cropped ribbed tank', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' +
+                "VALUES (5, 'top', 'bodysuit', 'M', 'F', 20, 'black', 'fairly used', 'Aritzia', 'black square neck bodysuit', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' +
+                "VALUES (6, 'top', 'bodysuit', 'M', 'F', 20, 'black', 'brand new', 'Aritzia', 'black square neck bodysuit', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' +
+                "VALUES (7, 'top', 'sweater', 'one-size', 'F', 12, 'blue', 'gently used', 'Brandy Melville', 'blue white sweater vest', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' +
+                "VALUES (8, 'outerwear', 'jacket', 'L', 'F', 12, 'black', 'like new', 'Zara', 'leather jacket', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' +
+                "VALUES (9, 'outerwear', 'coat', 'S (W)', 'F', 12, 'black', 'gently used', 'Nordstrom', 'black wrap coat', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+                stmt_str = ('INSERT INTO items (itemid, type, subtype, size, gender, price, color, condition, brand, "desc", posted, photolink, status)' +
+                "VALUES (10, 'shoes', 'heels/dress shoes', '8.5', 'F', 12, 'black', 'very used', 'Kate Spade', 'wrap heels', %s, 'https://www.google.com/', 0)")
+                cursor.execute(stmt_str, [testDate])
+
+
+            
                 #------------------------------------------------------- create users table
 
                 cursor.execute("DROP TABLE IF EXISTS users")
                 cursor.execute("CREATE TABLE users "
                     + '("netid" text, email text, joined timestamp, phone text)')
-                # cursor.execute("INSERT INTO authors (isbn, author) "
-                #     + "VALUES ('123','Kernighan')")
-                # cursor.execute("INSERT INTO authors (isbn, author) "
-                #     + "VALUES ('123','Pike')")
-                # cursor.execute("INSERT INTO authors (isbn, author) "
-                #     + "VALUES ('234','Kernighan')")
-                # cursor.execute("INSERT INTO authors (isbn, author) "
-                #     + "VALUES ('234','Ritchie')")
-                # cursor.execute("INSERT INTO authors (isbn, author) "
-                #     + "VALUES ('345','Sedgewick')")
+
+                now = datetime.utcnow()
+                testDate = now.strftime(f)
+
+                stmt_str = ("INSERT INTO users (netid, email, joined, phone) VALUES ('ishirai', 'ishirai@princeton.edu', %s, '6033066672')")
+                cursor.execute(stmt_str, [testDate])
+
+                now = datetime.utcnow() 
+                testDate = now.strftime(f)
+                stmt_str = ("INSERT INTO users (netid, email, joined, phone) VALUES ('kc42', 'kc42@princeton.edu', %s, '6503916837')")
+                cursor.execute(stmt_str, [testDate])
 
                 #------------------------------------------------------- create sellers table 
 
@@ -73,12 +133,7 @@ def main():
                 cursor.execute("INSERT INTO sellers "
                      + "(netid, itemid) VALUES "
                      + "('ishirai',0)")
-                # cursor.execute("INSERT INTO customers "
-                #     + "(custid, custname, street, zipcode) VALUES "
-                #     + "('222','Harvard','1256 Mass Ave','02138')")
-                # cursor.execute("INSERT INTO customers "
-                #     + "(custid, custname, street, zipcode) VALUES "
-                #     + "('333','MIT','292 Main St','02142')")
+             
 
                 #------------------------------------------------------- create reservations table
 
