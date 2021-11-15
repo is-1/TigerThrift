9513d867e56dcb7eb457d6b65999f68eaec6d650
Katie Chou <katielchou@gmail.com>
2021-11-15 16:02:54 -0500

    details fix

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ def profile():
 @app.route('/itemdetails', methods=['GET'])
 def itemdetails():
     # CasClient().authenticate()
-    html = render_template('itemdetails.html')
+    # html = render_template('itemdetails.html')
     itemid = request.args.get('itemid')
 
     item = item_details(itemid)
