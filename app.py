c818528f5d732d61595faa47dfccb3ac73908a6d
katelynr217 <70709121+katelynr217@users.noreply.github.com>
2021-12-14 02:17:39 -0500

    fix phone updates

--- app.py
+++ app.py
@@ -319,8 +319,8 @@ def success_edit():
         if str(user_phone) != "":
             print("went into if stmt")
             add_user_phone(netid=user_info['netid'], phone_number=user_phone)
-        else:
-            print("went into else stmt")
+        elif str(user_phone) == "" or user_phone == None:
+            print("went into elif stmt")
             add_user_phone(netid=user_info['netid'], phone_number="unknown")
         edit_item_success = edit_item_db(item_details, user_info)
         
